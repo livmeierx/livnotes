819bfeee7d704c3bf5abfdc7809f8595d6acbbe0
livmeierx <liv.meier13@gmail.com>
2022-07-18 18:54:51 -0400

    added middleware

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
+const allNotes = require('./db/db.json');
 
 const express = require('express');
 const app = express();
+
+// adding middleware
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 
 // function to create new note
 function createNote(body, notesArray) {
